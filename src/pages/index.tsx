0f9b8a347656f82115247ce616d9dcc3083e7229
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-04-29 16:08:52 +0200

    Added title to the project list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import ProjectList from "@/components/project/list/ProjectList"
 import Meta from "@/components/general/Meta"
 import appConfig from "@/config/appConfig"
 import ProjectListViewModel from "@/viewmodel/project/list/project_list_viewmodel"
+import styles from "@/styles/pages/Index.module.scss"
 import { useEffect } from "react"
 import PageContainer from "@/components/general/PageContainer"
 
@@ -16,6 +17,10 @@ const HomePage = () => {
     <>
       <Meta title={appConfig.website.name} />
       <PageContainer>
+        <div className={styles.titleContainer}>
+          <h1>made in (Flutter) Belgium</h1>
+          <p>This collection of applications developed, published or contributed by Belgian developers or companies using the Flutter technology. This showcases the innovative and creative work of local developers and companies in the mobile/web app space.</p>
+        </div>
         <ProjectList
           projects={viewModel.projects} />
       </PageContainer>
