248c40f05cb3b2e436ebfa8399a7e5cbd004a491
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-04-30 08:55:42 +0200

    #22-added better aria-labels

--- src/components/project/list/ProjectList.tsx
+++ src/components/project/list/ProjectList.tsx
@@ -19,7 +19,8 @@ const ProjectList = (props: ProjectListProps) => {
             <Link
                 className={style.addProjectContainer}
                 href='https://github.com/flutter-belgium/made_in_flutter_belgium_data/tree/main/examples/projects'
-                target='_blank'>
+                target='_blank'
+                aria-label="Add project">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
                     <rect width="256" height="256" fill="none" />
                     <line x1="40" y1="128" x2="216" y2="128" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" />
