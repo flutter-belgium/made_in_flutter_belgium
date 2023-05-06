c9b14f527e1ad2f01d5be22b2dabeb148bd961e9
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-05-06 08:55:18 +0200

    Updated the companies style & text (basic updates

--- src/pages/companies.tsx
+++ src/pages/companies.tsx
@@ -3,66 +3,28 @@ import appConfig from "@/config/appConfig"
 import { useEffect } from "react"
 import PageContainer from "@/components/general/PageContainer"
 import { useRouter } from "next/router"
-import Image from "next/image"
-import LinkSection from "@/components/general/link/LinkSection"
-import style from "@/styles/pages/Company.module.scss"
-import CompanyDetailViewModel from "@/viewmodel/company/detail/company_detail_viewmodel"
-import Button from "@/components/general/Button"
-import ProjectList from "@/components/project/list/ProjectList"
+import style from "@/styles/pages/Companies.module.scss"
 import { translations } from "@/util/locale/localization"
+import CompanyListViewModel from "@/viewmodel/company/list/company_list_viewmodel"
 
 const CompanyListPage = () => {
   const router = useRouter()
 
-  const viewModel = CompanyDetailViewModel()
+  const viewModel = CompanyListViewModel()
 
   useEffect(() => {
-    viewModel.init(router.query.name as string)
-  }, [router.query.name])
+    viewModel.init()
+  }, [])
 
-  const company = viewModel.company
+  const companies = viewModel.companies
   return (
     <>
       <Meta title={appConfig.website.name} />
       <PageContainer>
-        {
-          company == null ?
-            null :
-            <div className={style.container}>
-              <div className={style.infoContainer}>
-                <div className={style.titleContainer}>
-                  <Image
-                    className={company.useLogoInsteadOfTextTitle ? style.titleImage : style.titleText}
-                    height={company.useLogoInsteadOfTextTitle ? 64 : 48}
-                    width={company.useLogoInsteadOfTextTitle ? 64 : 48}
-                    alt={company.name}
-                    src={company.images.logoUrl} />
-                  {
-                    company.useLogoInsteadOfTextTitle ?
-                      null :
-                      <h1>{company.name}</h1>
-                  }
-                </div>
-                <p className={style.description}>{company.description}</p>
-                <Button
-                  title={translations.company_detail_website_btn}
-                  linkTo={company.links.website}
-                  target="_blank" />
-                <div className={style.phoneProjectListContainer}>
-                  <ProjectList
-                    projects={company.projects} />
-                </div>
-                <LinkSection
-                  title={translations.company_detail_dev_team_title}
-                  largeImages={false}
-                  links={viewModel.devTeamLinks} />
-              </div>
-              <div className={style.projectListContainer}>
-                <ProjectList
-                  projects={company.projects} />
-              </div>
-            </div>
-        }
+        <div className={style.titleContainer}>
+          <h1>{translations.companies_title}</h1>
+          <p>{translations.companies_subtitle}</p>
+        </div>
       </PageContainer>
     </>
   )
